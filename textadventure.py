78dbcd00e4300af07520601bcbae07dac0bccaa1
WaylonDN <nealwaylon304@gmail.com>
2021-11-05 15:48:22 -0400

    update textadventure.py

--- textadventure.py
+++ textadventure.py
@@ -11,30 +11,36 @@ while True:
     #checks to see if player class is a Warrior, Rogue, Mage, or Cleric
     #asks to confirm choice, if not then loops back to re-ask the question
     if player_class in ('Warrior','Rogue','Mage','Cleric'):
+
         #player class is warrior
         if player_class == 'Warrior':
             print("So, you're a Warrior? ")
             choice = input("y/n? ")
             if choice == "y" or "Y":
                 break
+
         #player class is rogue
         elif player_class == 'Rogue':
             print("So, you're a Rogue? ")
             choice = input("y/n? ")
             if choice == "y" or "Y":
                 break
+
         #player class is mage
         elif player_class == 'Mage':
             print("So, you're a Mage? ")
             choice = input("y/n? ")
             if choice == "y" or "Y":
                 break
+
         #player class is cleric
         elif player_class == 'Cleric':
             print("So, you're a Cleric? ")
             choice = input("y/n? ")
             if choice == "y" or "Y":
                 break
+
+    #otherwise print invalid choice
     else:
         print("Invalid choice!")          
    
@@ -62,23 +68,36 @@ while True:
         print("You think to yourself, where did he go?")
         print("You feel a sharp pain through your chest, as the stranger lunges a sword through your heart.")
         break
-    
+
     #this choice the player receives a map to a dungeon, and the stranger vanishes.
-    else:
+    elif choice == "2":
         print("The stranger reaches out to hand you something.")
         print("It's a map of a dungeon.")
+        
+        #choose yes or no to take the map
         choice = input("Do you take it? Y/N: ")
+
         #if choice is y or Y you take the map
         if choice == 'y' or 'Y':
-            print("The stranger hands you the map, he then vanishes in front of your eyes.")   
+            print("The stranger hands you the map, he then vanishes in front of your eyes.") 
+
         #you check out the map
             print("You look down at the map, you wonder if any of this is actually real.")
             print("You pinch yourself, no it's not a dream.")
             print("Then who was that mysterious stranger, and why did he give you a map?")
             break
-        
+
         #this choice the stranger just vanishes without giving you the map
-        elif choice == 'n' or 'N': 
+        elif choice == 'n' or 'N':
             print("The stranger just vanishes right before your eyes.")
+            break
+
+        #otherwise print invalid choice
+        else:
+            print("Invalid Choice!")
+
+    #otherwise print invalid choice
+    else:
+        print("Invalid Choice! ")
 
 print("As you study the map closer, you begin to feel some deja vu come over you.")
