23324bd0726e44cc748f9471148a9e58441e4471
WaylonDN <nealwaylon304@gmail.com>
2021-11-05 08:35:03 -0400

    updated textadventure.py with a loopback statement

--- textadventure.py
+++ textadventure.py
@@ -1,25 +1,45 @@
+#input your name here
 name = input("What is your name? ")
-player_name = name
-player_class = input("What class are you? ")
 
-if player_class in ('Warrior','Rogue','Mage','Cleric'):
+#defines the player name
+player_name = name
+
+while True:
+#input player class here
+    player_class = input("What class are you? ")
+
+    #checks to see if player class is a Warrior, Rogue, Mage, or Cleric
+    #asks to confirm choice, if not then loops back to reask the question
+    if player_class in ('Warrior','Rogue','Mage','Cleric'):
         if player_class == 'Warrior':
             print("So, you're a Warrior? ")
-            input("y/n? ")
+            choice = input("y/n? ")
+            if choice == "y":
+                break
 
         elif player_class == 'Rogue':
             print("So, you're a Rogue? ")
-            input("y/n? ")
+            choice = input("y/n? ")
+            if choice == "y":
+                break
 
         elif player_class == 'Mage':
             print("So, you're a Mage? ")
-            input("y/n? ")    
+            choice = input("y/n? ")
+            if choice == "y":
+                break
 
         elif player_class == 'Cleric':
             print("So, you're a Cleric? ")
-            input("y/n? ")
-else:
-    print("Invalid choice!")     
+            choice = input("y/n? ")
+            if choice == "y":
+                break
+    else:
+        print("Invalid choice!")          
+   
 
-print(f"Hello, {player_name}")
+
+#prints and states name and player class
+#prints "Welcome to the world of Lindus!"
 print(f"Hello, {player_name}. You are a {player_class}.")
+print("Welcome to the world of Lindus!")
