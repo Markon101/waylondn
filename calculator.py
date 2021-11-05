96b35cd39de3bf4cf8727f23abd84bd96e0b04fb
WaylonDN <nealwaylon304@gmail.com>
2021-11-05 14:40:51 -0400

    updated with a square function

--- calculator.py
+++ calculator.py
@@ -16,22 +16,30 @@ def multiply(x,y):
 def divide(x,y):
     return x/y
 
+#this function squares a number  
+def squared(x):
+    return x**2
+
 #print possible selections for calculator input types
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Squared")
 
 #creates a looping statement that breaks when you don't want to perform another calculation
 while True:
     #take input from user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
     #check if choice is one of the options
     if choice in ('1','2','3','4'):
         a = float(input("Enter first number: "))
         d = float(input("Enter second number:"))
+    #check if choice wants exponentials
+    if choice in ('5'):
+        a = float(input("Enter number: "))
 
         if choice == '1':
             print(a, "+", d, "=", add(a,d))
@@ -44,6 +52,9 @@ while True:
 
         elif choice == '4':
             print(a, "/", d, "=", divide(a,d))
+        
+        elif choice == '5':
+            print(a, "**", '2', "=", squared(a))
 
         #check if user wants another calculation
         #break the while loop if answer is no
