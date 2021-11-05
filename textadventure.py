47e75708fb7ff4a25abfd833c7b0fb1c323edd37
WaylonDN <nealwaylon304@gmail.com>
2021-11-05 09:31:03 -0400

    large update to textadventure.py

--- textadventure.py
+++ textadventure.py
@@ -11,28 +11,29 @@ while True:
     #checks to see if player class is a Warrior, Rogue, Mage, or Cleric
     #asks to confirm choice, if not then loops back to re-ask the question
     if player_class in ('Warrior','Rogue','Mage','Cleric'):
+        #player class is warrior
         if player_class == 'Warrior':
             print("So, you're a Warrior? ")
             choice = input("y/n? ")
-            if choice == "y":
+            if choice == "y" or "Y":
                 break
-
+        #player class is rogue
         elif player_class == 'Rogue':
             print("So, you're a Rogue? ")
             choice = input("y/n? ")
-            if choice == "y":
+            if choice == "y" or "Y":
                 break
-
+        #player class is mage
         elif player_class == 'Mage':
             print("So, you're a Mage? ")
             choice = input("y/n? ")
-            if choice == "y":
+            if choice == "y" or "Y":
                 break
-
+        #player class is cleric
         elif player_class == 'Cleric':
             print("So, you're a Cleric? ")
             choice = input("y/n? ")
-            if choice == "y":
+            if choice == "y" or "Y":
                 break
     else:
         print("Invalid choice!")          
@@ -44,3 +45,34 @@ while True:
 print(f"Hello, {player_name}. You are a {player_class}.")
 print("Welcome to the world of Lindus!")
 print("In this world you'll have to make many choices to overcome obstacles within your path.")
+
+#first actual lines of the game
+print("You're sitting in a small pub, you see a cloaked stranger sitting alone at a table.")
+print("You think to yourself... Why is nobody else noticing him sitting there?")
+print("The stranger looks up at you with glaring eyes piercing through you, you look away.")
+print("The stranger begins walking towards you reaching for something, what do you do?")
+
+#creates a looping statement in which you make your first real choice
+while True:
+    choice = input("1. Attack him. 2. Watch to see what he does. ")
+    
+    #this choice kills the player and stops the game
+    if choice == "1":
+        print("The stranger vanishes right before your eyes.")
+        print("You think to yourself, where did he go?")
+        print("You feel a sharp pain through your chest, as the stranger lunges a sword through your heart.")
+        break
+    
+    #this choice the player receives a map to a dungeon, and the stranger vanishes.
+    else:
+        print("The stranger reaches out to hand you something.")
+        print("It's a map of a dungeon.")
+        choice = input("Do you take it? Y/N? ")
+        if choice == "y" or "Y":
+            print("The stranger hands you the map, he then vanishes in front of your eyes.")
+            break
+        
+        #this choice the stranger just vanishes without giving you the map
+        else:
+            print("The stranger just vanishes right before your eyes.")
+            break
